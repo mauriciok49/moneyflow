12b09bf28fc14499b1fef012b3172f50a0b264f1
mauriciok49 <mauriciocerqueirak49@gmail.com>
2026-02-20 11:07:13 -0300

    Projeto inicial Spring Boot configurado

---
+++ src/test/java/com/mauricio/moneyflow/MoneyflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauricio.moneyflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
