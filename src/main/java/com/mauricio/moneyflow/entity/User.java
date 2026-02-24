8defc32dbad564894405d14df8b7304479770067
mauriciok49 <mauriciocerqueirak49@gmail.com>
2026-02-24 00:40:33 -0300

    feat: implementa entidade User Closes #4

--- src/main/java/com/mauricio/moneyflow/entity/User.java
+++ src/main/java/com/mauricio/moneyflow/entity/User.java
@@ -1,4 +1,31 @@
 package com.mauricio.moneyflow.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "password")
+@EqualsAndHashCode(of = "id")
+@Builder
 public class User {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 }
